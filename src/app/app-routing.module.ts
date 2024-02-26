c5dd849cb3d45d326039fd3793f67cd03c2294c0
emindemirhan <emindemirhan@icloud.com>
2024-02-27 01:45:59 +0300

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { BaseComponent } from './views/layout/base/base.component';
+import { AuthGuard } from './core/guard/auth.guard';
+
+
+const routes: Routes = [
+  { path:'auth', loadChildren: () => import('./views/pages/auth/auth.module').then(m => m.AuthModule) },
+  {
+    path: '',
+    component: BaseComponent,
+    canActivate: [AuthGuard],
+    children: [
+      {
+        path: 'dashboard',
+        loadChildren: () => import('./views/pages/dashboard/dashboard.module').then(m => m.DashboardModule)
+      },
+      
+     
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full' }, 
+      // { path: '**', redirectTo: 'dashboard', pathMatch: 'full' }
+    ]
+  },
+
+  { path: '**', redirectTo: 'error', pathMatch: 'full' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'top' })],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
